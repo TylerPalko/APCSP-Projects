63d5879d3b40356684d74b933c5700fce20753d8
Tyler Palko <88953270+TylerPalko@users.noreply.github.com>
2025-02-19 21:56:22 +0000

    Commit project

---
+++ MyApp2.py
@@ -0,0 +1,34 @@
+def fancyList(lst):
+    element = 1
+    for i in range(len(lst)):
+        print(f"{element}. {lst[element - 1]}")
+        element +=1
+    return ""
+tasks = [
+    "Buy groceries",
+    "Finish project report",
+    "Finish python project",
+    "Schedule a dentist appointment",
+    "Go for a run"
+]
+userChoice = 0
+print('Welcome to to-do list app!\n')
+while userChoice != 4:
+    print('Choose an option:\n')
+    print('(1) Read out your to do list')
+    print('(2) Remove an item')
+    print('(3) Add an item')
+    print('(4) Exit app')
+    userChoice = int(input())
+    
+    if userChoice == 1:
+        print()
+        print(fancyList(tasks))
+    if userChoice == 2:
+        print()
+        print(fancyList(tasks))
+        remItem = int(input('What item would you like to remove? (Type a number)'))-1
+        del tasks[remItem]
+    if userChoice == 3:
+        print()
+        tasks.append(input("What would you like to add? "))
