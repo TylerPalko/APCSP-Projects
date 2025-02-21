8bcb52c007016e34f75a48878e30813add744bd3
Tyler Palko <88953270+TylerPalko@users.noreply.github.com>
2025-02-21 00:13:44 +0000

    Place some code within procedure so I meet the project requirements (needed if statement in procedure)

--- MyApp2.py
+++ MyApp2.py
@@ -1,9 +1,20 @@
-def fancyList(lst):
-    element = 1
-    for i in range(len(lst)):
-        print(f"{element}. {lst[element - 1]}")
-        element +=1
-    return ""
+def listMethods(lst, choice):
+    if choice == 1:
+        element = 1
+        for i in range(len(lst)):
+            print(f"{element}. {lst[element - 1]}")
+            element +=1
+    elif choice == 2:
+        element = 1
+        for i in range(len(lst)):
+            print(f"{element}. {lst[element - 1]}")
+            element +=1
+            remItem = int(input('What item would you like to remove? (Type a number) '))-1
+            del tasks[remItem]
+    elif choice == 3:
+        print()
+        tasks.append(input("What would you like to add? "))
+    
 tasks = [
     "Buy groceries",
     "Finish project report",
@@ -18,16 +29,4 @@ while userChoice != 4:
     print('(2) Remove an item')
     print('(3) Add an item')
     print('(4) Exit app')
-    userChoice = int(input('Choose an option: '))
-    
-    if userChoice == 1:
-        print()
-        print(fancyList(tasks))
-    if userChoice == 2:
-        print()
-        print(fancyList(tasks))
-        remItem = int(input('What item would you like to remove? (Type a number) '))-1
-        del tasks[remItem]
-    if userChoice == 3:
-        print()
-        tasks.append(input("What would you like to add? "))
+    listMethods(tasks, int(input('Choose an option: ')))
