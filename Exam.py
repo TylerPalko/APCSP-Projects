ddca8d2e983e4761998f0f2eb0f7f4e01141f51c
Tyler Palko <88953270+TylerPalko@users.noreply.github.com>
2025-02-28 19:00:22 +0000

    Remove procedure and just repeat it instead (no functional change)

--- Exam.py
+++ Exam.py
@@ -1,13 +1,10 @@
-def fancyList(lst):
-    element = 1
-    for i in range(len(lst)):
-        print(f"{element}. {lst[element - 1]}")
-        element +=1
-        
 def options(item, burgers, meals, drinks):
     if item == 1:
         print()
-        fancyList(burgers)
+        element = 1
+        for i in range(len(burgers)):
+            print(f"{element}. {burgers[element - 1]}")
+            element +=1
         burgerType = int(input("Which burger do you want? "))
         print(f"\nYou ordered a {burgers[burgerType -1]}")
 
@@ -16,16 +13,25 @@ def options(item, burgers, meals, drinks):
 
     elif item == 3: 
         print()
-        fancyList(meals)
+        element = 1
+        for i in range(len(meals)):
+            print(f"{element}. {meals[element - 1]}")
+            element +=1
         mealType = int(input("Which meal do you want? "))
         print()
-        fancyList(drinks)
+        element = 1
+        for i in range(len(meals)):
+            print(f"{element}. {meals[element - 1]}")
+            element +=1
         drinkType = int(input("What drink do you want? "))
         print(f"\nYou ordered a {meals[mealType -1]} with a {drinks[drinkType-1]}")
 
     elif item == 4:
         print()
-        fancyList(drinks)
+        element = 1
+        for i in range(len(drinks)):
+            print(f"{element}. {drinks[element - 1]}")
+            element +=1
         drinkType = int(input("What drink do you want? "))
         print(f"\nYou ordered a {drinks[drinkType -1]}")
     
@@ -44,6 +50,9 @@ meals = ["Cheeseburger Meal", "Hamburger Meal", "Chicken Sandwhich meal (Halal)"
 drinks = ["Water", "Cola", "Sprite", "Orange Soda", "Diet Cola"]
 
 
-fancyList(menu)
+element = 1
+for i in range(len(menu)):
+    print(f"{element}. {menu[element - 1]}")
+    element +=1
 item = int(input("Choose an item number! "))
 options(item, burgers, meals, drinks)
